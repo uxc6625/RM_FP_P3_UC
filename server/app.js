feec25fc4aebb05df326e44acdccc8c255ee676c
Ulijah Charles <uxc6625@rit.edu>
2019-04-21 14:28:21 -0400

    app.js and handlebars work

---
+++ server/app.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DishFinder';
+
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+let redisURL = {
+  hostname: 'localhost',
+  port: 6379,
+};
+
+let redisPASS;
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
+}
+
+// pull in our routes
+const router = require('./router.js');
+
+const app = express();
+const foodDatabse = require('./foodDatabase');
+
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(session({
+  key: 'sessionid',
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS,
+  }),
+  secret: 'Cooked Asparagus',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
+}));
+const controlDBCall = (req, res, ref) => {
+// ERROR
+  const feedback = res;
+  res.on('Error', (err) => {
+    console.log(err);
+    feedback.statusCode = 404;
+    feedback.end();
+  });
+
+  // ADD
+  if (ref.pathname === '/find') {
+    foodDatabse.searchYummly(req, res);
+  }
+};
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+  return false;
+});
+
+app.post('/find', (req, res) => {
+  const ref = url.parse(req.url);
+  controlDBCall(req, res, ref);
+});
+
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
